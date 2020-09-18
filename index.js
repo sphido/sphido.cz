d1aebffa204922163e62ad8ed934b5c027afb90c
Roman Ožana <roman@omdesign.cz>
2020-09-18 20:26:41 +0200

    Fix CS

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env npx babel-node
 
-import {join} from "path";
-import {getPages} from "@sphido/core";
-import sitemap from "@sphido/sitemap";
-import {renderToFile, save} from "@sphido/nunjucks";
-import {link} from "@sphido/link";
-import frontmatter from "@sphido/frontmatter";
-import twemoji from "@sphido/twemoji";
-import {markdown, renderer} from "@sphido/markdown";
-import meta from "@sphido/meta";
-import {copy, outputFile} from "fs-extra";
-import globby from "globby";
+import {join} from 'path';
+import {getPages} from '@sphido/core';
+import sitemap from '@sphido/sitemap';
+import {renderToFile, save} from '@sphido/nunjucks';
+import {link} from '@sphido/link';
+import frontmatter from '@sphido/frontmatter';
+import twemoji from '@sphido/twemoji';
+import {markdown, renderer} from '@sphido/markdown';
+import meta from '@sphido/meta';
+import {copy, outputFile} from 'fs-extra';
+import globby from 'globby';
 
 const domain = new URL('https://sphido.org/');
 
@@ -19,26 +19,24 @@ renderer(
 		table: (header, body) => `<table class="table table-bordered table-striped bg-white m-1">${header}${body}</table>`,
 
 		image: (href, title, text) => {
-			const className = new URL(href, domain).hash.substring(1).replace(/[_]/g, ' ');
+			const className = new URL(href, domain).hash.slice(1).replace(/_/g, ' ');
 			return `<div class=" ${className ? className : 'd-flex justify-content-center my-1'}"><figure class="figure text-center w-75">
 			<a href="${href}" target="_blank"><img src="${href}" class="figure-img img-fluid rounded shadow" title="${title ? title : ''}" alt="${text ? text : ''}"/></a>		
-			<figcaption class="figure-caption text-center">${text}</figcaption></figure></div>`
+			<figcaption class="figure-caption text-center">${text}</figcaption></figure></div>`;
 		},
 
 		link: (href, title, text) => {
 			if (href.includes(domain.hostname)) {
-				return `<a href="${href}" title="${title ? title : ''}">${text}</a>`
-			} else {
-				return `<a href="${href}" title="${title ? title : ''}" target="_blank">${text}</a>`
+				return `<a href="${href}" title="${title ? title : ''}">${text}</a>`;
 			}
+
+			return `<a href="${href}" title="${title ? title : ''}" target="_blank">${text}</a>`;
 		}
 	}
 );
 
-
 (async () => {
 	try {
-
 		// 1. Get pages from directory
 
 		const pages = await getPages(
@@ -48,10 +46,9 @@ renderer(
 				twemoji,
 				markdown,
 				meta,
-				{save, link},
-			],
+				{save, link}
+			]
 		);
-
 
 		// 2. Generate single pages...
 
@@ -67,13 +64,13 @@ renderer(
 		);
 
 		// 4. copy static content
+
 		const files = await globby(['content/**/*.*', '!**/*.{md,html}']);
 		for await (const file of files) {
-			await copy(file, file.replace(/^[\w]+/, 'public'));
+			await copy(file, file.replace(/^\w+/, 'public'));
 		}
 
 		await renderToFile('public/404.html', 'content/404.html');
-
 	} catch (error) {
 		console.error(error);
 	}
