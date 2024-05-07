c1477a2e4b073a82234173418ca7e935c7e2b331
Roman Ožana <roman@ozana.cz>
2024-05-07 08:52:51 +0200

    Fix sidebar

--- src/get-sidebar.js
+++ src/get-sidebar.js
@@ -8,7 +8,7 @@ export function getSidebar(pages, active) {
 		<ul class="divide-y divide-gray-200 dark:divide-gray-900">
 			${docs.map(page => `
 			<li>
-				<a href="${page.slug}" title="${page.title}" class="flex px-3 py-3.5 hover:text-sky-600 dark:hover:text-sky-500 ${page.slug === active ? 'font-semibold text-sky-600 dark:text-sky-500' : ''}" data-active="${page.slug == active}">
+				<a href="${page.slug}" title="${page.title}" class="flex px-3 py-3.5 hover:text-sky-600 dark:hover:text-sky-500 ${page.slug === active ? 'font-semibold text-sky-600 dark:text-sky-500' : ''}" data-active="${page.slug === active}">
 					${page.name === 'index' ? 'Home' : page.title}
 				</a>
 		</li>`).join('')}	
